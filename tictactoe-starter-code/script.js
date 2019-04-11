58945bd8188172dd3cc1046cf5570794c970b9d6
Tech-A08 <tech-a08@MacBook-Air-3.local>
2019-04-11 19:37:57 +0100

    reset button is working

--- tictactoe-starter-code/script.js
+++ tictactoe-starter-code/script.js
@@ -1,31 +1,8 @@
 $(function(event){
 
-var turn = "X"
+  var turn = "X"
 
-// $("table").on("click",function(){
-//   $("td").each(function(index, grid){
-//     $(grid).on("click", function(){
-//     })
-  // })
-  // if(turn == "first"){
-  //   $("td").each(function(index, grid){
-  //   $(grid).on("click", function(){
-  //   $(grid).html("X")
-  //   console.log("first player clicked")
-  //   turn = "second"
-  // })
-  // })
-  // }
-  // else {
-  //   $("td").each(function(index, grid){
-  //
-  //   $(grid).on("click", function(){
-  //   $(grid).html("O")
-  //   console.log("second player clicked")
-  //   turn = "first"
-  // })
-  // })
-  // }
+
   function addClickListeners(){
     $("td").each(function(index, grid){
       $(grid).on("click", function(){
@@ -39,45 +16,9 @@ var turn = "X"
       })
     })
   }
-addClickListeners()
+  addClickListeners()
+
+  $("#reset").on("click", function(){
+    $("td").html("");
+  })
 })
-
-
-
-
-
-
-
-// })
-
-
-
-
-
-
-// $(function(event){
-
-
-  // As a user I expect the cross to appear in the square that I click when its my turn and be able to click it again
-  // $("td").each(function(index, grid){
-  //   $(grid).on("click", function(){
-  //     $(grid).html(ticTac)
-  //   })
-  // })
-  // // I expect the computer to put a Circle somewhere and be able to click again after computer turn
-  // $("td").each(function(index, grid){
-  //   if(grid == "" && grid[i] == "X"){
-  //     $(grid).on("click", function(){
-  //       $(grid).html("O")
-  //     })
-  //   }
-  // })
-
-
-
-
-
-
-
-
-// })
